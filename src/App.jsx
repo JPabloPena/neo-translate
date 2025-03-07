822ccef39da1cfceab03462d84d2e7a7d35836fb
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-06 21:14:59 -0500

    chore: initalize project with basic structure and configuration files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <>
+      <div className='h-screen bg-bg-primary'>
+        <p className='text-primary'>Hola</p>
+      </div>
+    </>
+  )
+}
+
+export default App
