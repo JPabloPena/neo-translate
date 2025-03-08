bc6535c1a68ae922bfca647ab6f07ca51cacb171
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-08 18:26:21 -0500

    feat: enhance logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import Moon from '../icons/Moon'
 function Header () {
   return (
     <header className='text-primary h-[70px] fixed top-0 z-10 w-full px-8 bg-bg-primary flex justify-between items-center'>
-      <h1 className='flex gap-2 items-center text-2xl font-semibold'>
-        <Language className='size-7 stroke-1.5' />
+      <h1 className='flex gap-1 items-center text-2xl italic font-semibold'>
+        <Language className='size-7 stroke-2' />
         <span>NeoTranslator</span>
       </h1>
       <section>
