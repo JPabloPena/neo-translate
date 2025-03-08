d808c05539e11e8810914956f6a78f5dd6a5104a
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-08 12:16:35 -0500

    feat: style buttons for the selected language

--- src/components/TranslatorOutput.jsx
+++ src/components/TranslatorOutput.jsx
@@ -2,20 +2,46 @@ import SpeakerWave from '../icons/SpeakerWave'
 import Copy from '../icons/Copy'
 import Exchange from '../icons/Exchange'
 import ChevronDown from '../icons/ChevronDown'
+import { useContext, useRef, useState } from 'react'
+import { TranslatorContext } from '../context/Translator'
 
 function TranslatorOutput () {
+  const { setTranslationLang } = useContext(TranslatorContext)
+  const [activeLangButton, setActiveLangButton] = useState('btn-to-es')
+  const selectRef = useRef(null)
+
   const activeLanguageClass = 'bg-accent'
+  const selectOptions = ['btn-to-fr', 'btn-to-ca']
+
+  const handleSelectFocus = () => {
+    selectRef.current.selectedIndex = 0
+    setActiveLangButton('btn-to-autodetect')
+  }
+
+  const handleActiveLanguage = e => {
+    let newToTranslateActiveButton
+    if (e.target.tagName === 'SELECT') {
+      newToTranslateActiveButton = `btn-to-${e.target.value}`
+    } else {
+      newToTranslateActiveButton = e.target.id
+    }
+    setActiveLangButton(newToTranslateActiveButton)
+
+    const newToTranslateLang = e.target.value
+    setTranslationLang(newToTranslateLang)
+  }
 
   return (
     <div className='h-96 w-[620px] bg-bg-secondary border-1 border-accent rounded-2xl p-5 text-white flex flex-col transition'>
       <section className='flex justify-between items-center border-b-1 border-accent pb-3'>
         <div className='flex gap-5'>
-          <button className='lang-button'>English</button>
-          <button className={`lang-button ${activeLanguageClass}`}>Spanish</button>
+          <button id='btn-to-en' className={`lang-button ${activeLangButton === 'btn-to-en' ? activeLanguageClass : ''}`} value='en' onClick={handleActiveLanguage}>English</button>
+          <button id='btn-to-es' className={`lang-button ${activeLangButton === 'btn-to-es' ? activeLanguageClass : ''}`} value='es' onClick={handleActiveLanguage}>Spanish</button>
           <div className='relative items-center'>
-            <select className='change-lang-button'>
-              <option className='bg-accent'>French</option>
-              <option className='bg-accent'>Catalan</option>
+            <select ref={selectRef} className={`change-lang-button ${selectOptions.includes(activeLangButton) ? activeLanguageClass : ''}`} onChange={handleActiveLanguage} onFocus={handleSelectFocus}>
+              <option className='bg-accent' disabled>Select</option>
+              <option id='btn-to-fr' className='bg-accent' value='fr' selected>French</option>
+              <option id='btn-to-ca' className='bg-accent' value='ca'>Catalan</option>
             </select>
             <ChevronDown className='size-3 pointer-events-none absolute inset-y-4 right-3 stroke-3' />
           </div>
