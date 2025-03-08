af487c12335ce6b73675adecc460c2b9f25e28e9
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-08 15:09:55 -0500

    feat: fetch API and set results in states

--- src/context/Translator.jsx
+++ src/context/Translator.jsx
@@ -1,19 +1,45 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const TranslatorContext = createContext()
 
-const API_URL = 'https://api.mymemory.translated.net/get?q=Hello%20World!&langpair=Autodetect|it'
+const API_URL = 'https://api.mymemory.translated.net'
 
 function TranslatorProvider ({ children }) {
   const [toTranslate, setToTranslate] = useState('')
+  const [translation, setTranslation] = useState('')
   const [toTranslateLang, setToTranslateLang] = useState('autodetect')
   const [translationLang, setTranslationLang] = useState('es')
+
+  useEffect(() => {
+    async function fetchTranslation () {
+      if (toTranslate.length === 0) return
+
+      const encodedQuery = encodeURIComponent(toTranslate)
+      const response = await fetch(`${API_URL}/get?q=${encodedQuery}&langpair=${toTranslateLang}|${translationLang}`)
+
+      if (!response.ok) throw new Error('Error searching your translation')
+
+      const translationData = await response.json()
+      const { matches: [{ translation: translatedText }] } = translationData
+      setTranslation(translatedText)
+
+      if (toTranslateLang === 'autodetect') {
+        const { responseData: { detectedLanguage } } = translationData
+        setToTranslateLang(detectedLanguage)
+      }
+    }
+
+    fetchTranslation()
+  }, [toTranslate, toTranslateLang, translationLang])
 
   return (
     <TranslatorContext.Provider value={{
       toTranslate,
       setToTranslate,
-      setToTranslateLang
+      translation,
+      toTranslateLang,
+      setToTranslateLang,
+      setTranslationLang
     }}
     >
       {children}
