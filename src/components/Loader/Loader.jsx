36c5a2e4d806ec6325d88641b555def6c796049c
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-09 13:38:05 -0500

    feat: add Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+/* Loader taken from: https://www.loadership.com/loaders/dot_linear_bounce */
+import './Loader.css'
+
+function Loader () {
+  return (
+    <div className='loadership_UZHHE mt-2'>
+      <div className='bg-placeholder' />
+      <div className='bg-placeholder' />
+      <div className='bg-placeholder' />
+      <div className='bg-placeholder' />
+    </div>
+  )
+}
+
+export default Loader
