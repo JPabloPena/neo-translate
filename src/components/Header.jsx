aa3d3bbb10eded961fc0122acb1de123dbd05d39
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-09 01:49:36 -0500

    fix: fix app name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header () {
     <header className='text-primary h-[70px] fixed top-0 z-10 w-full px-8 bg-bg-primary flex justify-between items-center'>
       <h1 className='flex gap-1 items-center text-2xl italic font-semibold'>
         <Language className='size-7 stroke-2' />
-        <span>NeoTranslator</span>
+        <span>NeoTranslate</span>
       </h1>
       <section className='flex gap-3'>
         <a href='https://mymemory.translated.net/doc/spec.php' target='_blank' className='action-button-white flex items-center gap-1' rel='noreferrer' disabled>
