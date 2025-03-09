de4f2bc29c206d027ca0821f23f4eb801e45cb55
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-03-09 01:47:42 -0500

    feat: add CopyToClipboard component and new button style

---
+++ src/components/CopyToClipboard.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import Copy from '../icons/Copy'
+import DocumentCheck from '../icons/DocumentCheck'
+import copyToClipboard from '../utils/copyToClipboard'
+
+function CopyToClipboard ({ text }) {
+  const [isSuccess, setIsSuccess] = useState(false)
+  console.log({ text })
+
+  const handleButtonClick = () => {
+    if (text.length === 0) return
+
+    copyToClipboard(text)
+    setIsSuccess(true)
+    setTimeout(() => {
+      setIsSuccess(false)
+    }, 1500)
+  }
+
+  return (
+    <>
+      {isSuccess
+        ? (
+          <button className='success-button'>
+            <DocumentCheck className='size-5' />
+          </button>
+          )
+        : (
+          <button className='action-button' onClick={handleButtonClick}>
+            <Copy className='size-5' />
+          </button>
+          )}
+    </>
+  )
+}
+
+export default CopyToClipboard
